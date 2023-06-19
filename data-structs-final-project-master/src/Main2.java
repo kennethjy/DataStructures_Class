059cd946d2de3f36257fc013453886bf53cd540b
kenneth <kennethjayadiyu@gmail.com>
2023-06-19 23:11:54 +0700

    push

--- data-structs-final-project-master/src/Main2.java
+++ data-structs-final-project-master/src/Main2.java
@@ -52,31 +52,30 @@ public class Main2 {
         while (storage.Stock.size() < num) {
             itemName = new StringBuilder();
             while (itemName.length() < random.nextInt()) {
-                itemName.append((char) random.nextInt(26)).append("a");
+                itemName.append((char) random.nextInt(26) + 'a');
             }
             double prevTime = System.nanoTime();
             storage.initializeItem(String.valueOf(itemName));
             time = System.nanoTime() - prevTime;
         }
-        System.out.println(time + "ns");
+        System.out.println("Add entry: " + time + "ns");
         Object[] keys = storage.Stock.keySet().toArray();
         for (int i = 0; i < num; i++) {
-            double prevTime = System.nanoTime();
+
             String item = keys[random.nextInt(keys.length)].toString();
             int amt = random.nextInt();
             storage.addRequest(item, amt, "Kenneth", new Date());
-            time = System.nanoTime() - prevTime;
         }
-        System.out.println(time + "ns");
-
-        LinkedList<String> requests = storage.getRequests();
         double prevTime = System.nanoTime();
+        LinkedList<String> requests = storage.getRequests();
+        int i = random.nextInt(requests.size());
+        System.out.println(requests.get(i));
+        time = System.nanoTime() - prevTime;
+        System.out.println("Find entry: " + time + "ns");
+        prevTime = System.nanoTime();
         storage.approveRequest(requests.get(random.nextInt(requests.size())).split(" ")[0]);
         time = System.nanoTime() - prevTime;
-        System.out.println(time + "ns");
-        for(int i = 1; i < num - 1; i++) {
-            storage.approveRequest(requests.get(random.nextInt(requests.size())).split(" ")[0]);
-        }
+        System.out.println("Remove entry: " + time + "ns");
     }
     public static void benchmarkLL(){
         StorageLL storage = new StorageLL();
@@ -89,13 +88,13 @@ public class Main2 {
         while (storage.Stock.size() < num) {
             itemName = new StringBuilder();
             while (itemName.length() < random.nextInt()) {
-                itemName.append((char) random.nextInt(26)).append("a");
+                itemName.append((char) random.nextInt(26) + 'a');
             }
             double prevTime = System.nanoTime();
             storage.initializeItem(String.valueOf(itemName));
             time = System.nanoTime() - prevTime;
         }
-        System.out.println(time + "ns");
+        System.out.println("Add entry: " + time + "ns");
         LinkedList<String> keys = new LinkedList<>();
         for (Item item: storage.Stock){
             keys.add(item.name);
@@ -105,17 +104,16 @@ public class Main2 {
             String item = keys.get(random.nextInt(keys.size()));
             int amt = random.nextInt();
             storage.addRequest(item, amt, "Kenneth", new Date());
-            time = System.nanoTime() - prevTime;
         }
-        System.out.println(time + "ns");
-
-        LinkedList<String> requests = storage.getRequests();
         double prevTime = System.nanoTime();
+        LinkedList<String> requests = storage.getRequests();
+        int i = random.nextInt(requests.size());
+        System.out.println(requests.get(i));
+        time = System.nanoTime() - prevTime;
+        System.out.println("Find entry: " + time + "ns");
+        prevTime = System.nanoTime();
         storage.approveRequest(requests.get(random.nextInt(requests.size())).split(" ")[0]);
         time = System.nanoTime() - prevTime;
-        System.out.println(time + "ns");
-        for(int i = 1; i < num - 1; i++) {
-            storage.approveRequest(requests.get(random.nextInt(requests.size())).split(" ")[0]);
-        }
+        System.out.println("Remove entry: " + time + "ns");
     }
 }
